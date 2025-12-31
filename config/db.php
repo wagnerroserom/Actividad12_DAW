04253cc67862bd9ebf2784534ce9a795ae6c9117
antonioroserom <wrosero9059@utm.edu.ec>
2025-12-30 22:37:13 -0500

    Se inserta bloque de código para crear la conexión con la base de datos

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+// Configuración de la conexión a la base de datos MySQL utilizando XAMPP.
+$host = 'localhost';            // Servidor de la base de datos
+$usuario_db = 'root';           // Usuario por defecto de MySQL en XAMPP
+$contraseña_db = '';
+$nombre_db = 'turnosalud_montecristi';  // Nombre de la base de datos
+
+// Crea la conexión
+$conexion = new mysqli($host, $usuario_db, $contrasena_db, $nombre_db);
+
+// Verifica la conexión
+if ($conexion->connect_error) {
+    die("Error de conexión a la base de datos: " . $conexion->connect_error);
+}
+
+// Establece la codificación UTF-8 para evitar problemas con caracteres especiales
+$conexion->set_charset("utf8");
+?>
